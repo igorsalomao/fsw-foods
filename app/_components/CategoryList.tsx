801f480f2235b446341071b2aa710fc7335cb2ba
Igor Salomão <igorsalomao13@gmail.com>
2024-04-30 13:11:01 -0300

    feat: add category item & list components

--- app/_components/CategoryList.tsx
+++ app/_components/CategoryList.tsx
@@ -1,7 +1,15 @@
-const CategoryList = () => {
-  // pegar as categorias do bd
-  // renderizar um item para cada categoria
-  return "";
+import { db } from "../_lib/prisma";
+import CategoryItem from "./CategoryItem";
+
+const CategoryList = async () => {
+  const categories = await db.category.findMany({});
+  return (
+    <div className="flex overflow-x-scroll">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
 };
 
 export default CategoryList;
