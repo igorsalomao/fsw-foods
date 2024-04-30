4c13a00e5f92ba7491ea604d696d9dc1c30f7058
Igor Salomão <igorsalomao13@gmail.com>
2024-04-30 11:42:41 -0300

    feat: add initial search component

---
+++ app/_components/Search.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { SearchIcon } from "lucide-react";
+
+const Search = () => {
+  return (
+    <div className="flex gap-2">
+      <Input placeholder="Buscar Restaurantes" className="border-none" />
+      <Button size="icon">
+        <SearchIcon size={20} />
+      </Button>
+    </div>
+  );
+};
+
+export default Search;
